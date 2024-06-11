bb279d07678f03795598b8f9ae799a798ed64bd6
masud4657 <masudpfrnt@gmail.com>
2024-06-11 12:10:56 +0600

    added the verify token and verify admin api to secure the website.

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ const cors = require('cors');
 const port= process.env.PORT||6003
 
 // middleware
-app.use(cors())
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
+
+// Verify Token Middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  console.log(token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 
 
@@ -25,7 +48,11 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+// const cookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+// };
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +60,51 @@ async function run() {
     const classCollection= client.db('skillTrackDb').collection('classes')
     const userCollection= client.db('skillTrackDb').collection('users')
     const teacherCollection= client.db('skillTrackDb').collection('teachers')
+
+
+    // verify admin middleware
+    const verifyAdmin = async (req, res, next) => {
+      console.log('hit for the admin ')
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await userCollection.findOne(query)
+      console.log('user role' ,result?.role)
+      if (!result || result?.role !== 'admin')
+        return res.status(401).send({ message: 'unauthorized access!!' })
+
+      next()
+    }
+
+    // auth related api OR creating Token
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    //clearing Token OR Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
     // save classes in the mongoDb 
     app.post('/class',async(req,res)=>{
       const classData=req.body
@@ -93,7 +165,7 @@ async function run() {
 
 
     // get all users from mongoDb
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyToken,verifyAdmin, async(req,res)=>{
       const result= await userCollection.find().toArray()
       res.send(result)
     })
